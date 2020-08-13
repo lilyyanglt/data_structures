4af9f53a4200851dc9a3ca883ff7a75b9c147bab
Lily <lilyyanglt@gmail.com>
2020-08-13 16:42:58 -0700

    Added min and max method to bst tree implementation

--- cpp/non_linear/binarySearchTree/binarySearchTree.cpp
+++ cpp/non_linear/binarySearchTree/binarySearchTree.cpp
@@ -12,29 +12,66 @@ class BST {
   private:
     TreeNode *root;
 
-  public:
-    BST() : root(nullptr) {}
-
-    TreeNode* getRoot() {
-      return root;
-    }
-
-    void add2(int x, TreeNode *& r) {
+  protected: 
+    /*
+      This is a protected function that's not meant to be called outside of the instance
+      @param r - is a reference to the pointer passed
+      @param x - is the value of the node
+    */
+    void add(int x, TreeNode *& r) {
       if (r == nullptr) {
         TreeNode *newNode = new TreeNode(x);
         r = newNode;
         return;
       } else {
         if (r->data >= x) {
-          add2(x, r->left);
+          add(x, r->left);
         } else {
-          add2(x, r->right);
+          add(x, r->right);
         }
       }
     }
 
-    void add2(int x) {
-      add2(x, root);
+  public:
+    BST() : root(nullptr) {}
+
+    /*
+      @return copy of the pointer variable to root
+    */
+
+    TreeNode* getRoot() {
+      return root;
+    }
+
+    /*
+      This is the function available to users
+      @param x - the data of the new node to be added to the tree
+    */
+    void add(int x) {
+      add(x, root);
+    }
+
+    /*
+     Find the min of the tree recursively
+     @param r - is the pointer type variable
+     @return - the min value of the tree in int
+    */
+
+    int min(TreeNode * r) {
+      if(r->left == nullptr) return r->data;
+
+      return min(r->left);
+    }
+
+    /*
+      Finds the max of the tree recursively
+      @param r - this is the pointer type variable
+      @return - the max value of the bst tree
+    */
+    int max(TreeNode * r) {
+      if(r->right == nullptr) return r->data;
+
+      return max(r->right);
     }
 
 };
@@ -57,13 +94,18 @@ int main(void) {
 
   BST binarySearchTree;
 
-  binarySearchTree.add2(10);
-  binarySearchTree.add2(20);
-  binarySearchTree.add2(3);
-  binarySearchTree.add2(14);
-  binarySearchTree.add2(17);
+  binarySearchTree.add(10);
+  binarySearchTree.add(20);
+  binarySearchTree.add(3);
+  binarySearchTree.add(14);
+  binarySearchTree.add(17);
 
   inorder(binarySearchTree.getRoot());
   std::cout << std::endl;
+
+  std::cout << "Min value is: " << binarySearchTree.min(binarySearchTree.getRoot()) << std::endl;
+
+  std::cout << "Root value is: " << binarySearchTree.getRoot()->data << std::endl;
+
   return 0;
 }
